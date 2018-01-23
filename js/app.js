eb66e2f3ec1f1e5660d0874aadbc28f2b477cbbb
Adriana Becerril <Adriana@MacBook-Air-de-Arisbeth.local>
2018-01-23 03:04:45 -0600

    funcion para generar codigo random, lista

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+//Funciones para direccionar botones a las otras views
 $("#btn-sing-up").click(function() {
     document.location.href = "view-phone.html";
 });
@@ -14,27 +15,54 @@ $("#btn-next-3").click(function() {
     document.location.href = "registry.html";
 });
 
+
+
 //Llamando a nuestros elementos de html que usaremos
 var $phoneInput = $("#phone-input");
 var $buttonNext = $("#btn-next");
+var $buttonResend = $(".btn-resend");
+var $drop = $(".dropdown");
+
 
 //Funciones para validar que el usuario ingrese datos correctos para activar el botón next
 /*var loadPage = function() {
-    //$phoneInput.keyup(validatePhone);
-    $("#btn-next").submit(addPhone);
+    $phoneInput.keyup(validatePhone);
+    $(".dropdown").submit(addPhone);
+
 };
 
+var validatePhone = function() {
+    if ($(this).val().trim().length > 0) {
+        $("#btn-next").removeAttr("disabled");
+    } else {
+        $("#btn-next").attr("disabled", true);
+    }
+};
 
 var addPhone = function(e) {
     e.preventDefault();
     var phone = $phoneInput.val();
-    $("#phoneInput")[0].reset();
+    $drop[0].reset();
 };
 
-var validatePhone = function() {
-    if ($("#phone-input").val().trim().length > 0) {
-        $("#btn-next").prop("disabled", false);
-    } else {
-        $("#btn-next").prop("disabled", true);
+//Funciones para generar código random
+
+var code = [];
+
+function random() {
+    var rdm = Math.floor(Math.random() * 10);
+    code.push(rdm);
+    return rdm;
+}
+
+function codeRandom() {
+    var threeNumber = "";
+    for (var i = 0; i < 3; i++) {
+        threeNumber += random();
     }
-};*/
+    var codePhone = alert("Lab-" + threeNumber);
+}
+$buttonNext.click(codeRandom);
+$buttonResend.click(codeRandom);
+
+$(document).ready(loadPage);*/
